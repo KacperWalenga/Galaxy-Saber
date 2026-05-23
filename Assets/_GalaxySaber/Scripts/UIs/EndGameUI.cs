6e872633146b8cb66df00432bc0b47a51cf58727
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2026-05-23 23:43:04 +0200

    WIP

--- Assets/_GalaxySaber/Scripts/UIs/EndGameUI.cs
+++ Assets/_GalaxySaber/Scripts/UIs/EndGameUI.cs
@@ -1,4 +1,5 @@
 ﻿using _GalaxySaber;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,12 @@ public class EndGameUI : MonoBehaviour
 {
     [SerializeField] private GameObject canvas;
     [SerializeField] private Button continueButton;
+
+    [Header("Summary")]
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text hittedNotesText;
+    [SerializeField] private TMP_Text missedNotesText;
+    [SerializeField] private TMP_Text ratingText;
 
     private void Awake()
     {
@@ -30,6 +37,12 @@ public class EndGameUI : MonoBehaviour
         var hittedNotes = scoreManager.HittedNotesCount;
         var missedNotes = scoreManager.MissedNotesCount;
         var rating = scoreManager.Rating;
+        var maxRating = scoreManager.MaxRating;
+        
+        scoreText.text = $"Score: {score}";
+        hittedNotesText.text = $"Hitted Notes: {hittedNotes}";
+        missedNotesText.text = $"Missed Notes: {missedNotes}";
+        ratingText.text = $"Rating: {rating} / {maxRating}";
         
         canvas.SetActive(true);
     }
